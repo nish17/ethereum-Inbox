87428655263e9dc05ebe18b08b7e33bce5933e26
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-03 22:28:42 +0530

    add tests for default and updating the message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,19 +5,30 @@ const { interface, bytecode } = require("../compile");
 const web3 = new Web3(ganache.provider());
 
 let accounts, inbox;
-
+const INITIAL_MESSAGE = "hi there";
+const UPDATE_MESSAGE = "Ethereum";
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["hi there!"]
+      arguments: [INITIAL_MESSAGE]
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("can update the message", async () => {
+    await inbox.methods.setMessage(UPDATE_MESSAGE).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(UPDATE_MESSAGE, message);
   });
 });
