fbf528179545926972877b8251e929d8bcebb178
Nimesh Solanki <nimesh3536@gmail.com>
2018-09-03 23:46:28 +0530

    add deploy code to rinkey test network

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWallerProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWallerProvider(
+  "december bomb print venue quantum balcony model slogan trouble blade arena believe",
+  "https://rinkeby.infura.io/v3/aa18450592954bd99ed379aa3c6ee17f"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(`Attempting to deploy from account ${accounts[0]}`);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [`Hello world`]
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log(`Contract deployed to ${result.options.address}`);
+};
+deploy();
